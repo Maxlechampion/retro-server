d24cdf4e1f6c0676fe95bcd6a3c84cae61f81acc
Maxlechampion <ahoemile@gmail.com>
2024-06-10 19:25:41 +0200

    suite de la création de la base de données

---
+++ migrations/20240610170538-create-message.js
@@ -0,0 +1,50 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Messages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      idUsers: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references:{
+          model:'users',
+          key:'id'
+
+        }
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      attachment: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      likes: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Messages');
+  }
+};
